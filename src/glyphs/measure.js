c081e89496175d3edaf73705f03053e56ef49fc5
Dave Kilian <dave.kilian@gmail.com>
2012-11-20 17:24:06 -0500

    Pitch-based note positioning

--- src/glyphs/measure.js
+++ src/glyphs/measure.js
@@ -21,6 +21,28 @@
         };
     }
 
+    function pitchDelta(pitch, clef) {
+        var originNote, originPitch;
+
+        if (clef == 'treble') {
+            originNote = 'F'.charCodeAt(0);
+            originPitch = 5;
+        } else {
+            console.log('NYI: note pitch on ' + clef + ' clef');
+        }
+
+        var note = pitch.charCodeAt(0);
+        var octave = parseInt(pitch.charAt(1));
+
+        return 7 * (octave - originPitch) + (note - originNote);
+    }
+
+    function pitchOffset(delta) {
+        var s = Notate.settings;
+        console.log(s.STAFF_LINE_SPACING);
+        return -delta * .5 * s.STAFF_LINE_SPACING + 0.5;
+    }
+
     Notate.layoutCallback['measure'] = function(measure) {
         var s = Notate.settings;
         var x = s.NOTE_SPACING;
@@ -28,7 +50,7 @@
         for (var i = 0; i < measure.children.length; ++i) {
             var note = measure.children[i];
             note.x = x;
-            note.y = 20.5;    // TODO determine based on pitch
+            note.y = pitchOffset(pitchDelta(note.pitch, 'treble'));
 
             x += s.NOTE_SPACING;
         }
